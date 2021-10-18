26533ef6e5941daae11af0e4b588d8866b3b15c9
sam FM <samfm@bleepBlort.local>
2021-10-18 16:19:12 -0700

    Header & Footer

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+function RenderCampsite( {campsite} ) {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>{props.campsite.name}</h2>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
+                <RenderCampsite campsite={props.campsite} />
+                <RenderComments comments={props.comments} />
+            </div>
+        </div>
+    );
+}
+
+function RenderComments( {comments} ) {
+    if(comments) {
+        return(
+            <div className="col-md-5 m-1">
+                <h4>Comments</h4>
+                { comments.map(comment => { 
+                    return(
+                        <div key={comment.id}>
+                            <p>{comment.text} <br /> 
+                            -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                            </p>
+                        </div>
+                    )
+                } )}
+            </div>
+        )
+    }
+}
+    
+function CampsiteInfo(props) {
+    if (props.campsite) {
+        return (
+            <div className="container">
+                <div className="row">
+                        <RenderCampsite campsite={props.campsite} />
+                        <RenderComments comments={props.campsite.comments} />
+                </div>
+            </div>    
+        );
+    }
+    return <div />;
+}
+
+export default CampsiteInfo;
---
+++ src/components/DirectoryComponent.js
@@ -0,0 +1,45 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+
+function renderDirectoryItem( campsite, onClick ) {
+    return (
+        <Card onClick={() => onClick(campsite.id)}>
+            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+            <CardImgOverlay>
+                <CardTitle>{campsite.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
+
+function Directory(props) {
+ 
+    const directory = props.campsites.map(campsite => {
+        return (
+            <div key={campsite.id} className="col-md-5 m-1">
+                <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+            </div>
+        );
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Directory</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Directory</h2>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
+                {directory}`
+            </div>
+        </div>
+    );
+}
+
+export default Directory;
