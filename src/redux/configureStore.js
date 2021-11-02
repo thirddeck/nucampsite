e3cade0d9f4f9a29d360898a0b923c1f8099538f
Sam Armstrong <38031922+thirddeck@users.noreply.github.com>
2021-11-02 01:56:24 -0500

    Week 5 Workshop

---
+++ src/redux/configureStore.js
@@ -0,0 +1,29 @@
+import {createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
+
+import { Campsites } from './campsites';
+import { Comments } from './comments';
+import { Partners } from './partners';
+import { Promotions } from './promotions';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        combineReducers({
+            campsites: Campsites,
+            comments: Comments,
+            partners: Partners,
+            promotions: Promotions,
+            ...createForms({
+                feedbackForm: InitialFeedback
+            })
+        }),  
+        composeWithDevTools(    
+        applyMiddleware(thunk, logger))
+    );
+
+    return store;
+}
